02020290c9b950a5e2e451df51e168daae5fe51b
antoine <antoine@huchet.me>
2018-01-22 15:52:49 +0100

    Updated parser

--- parser.py
+++ parser.py
@@ -3,15 +3,22 @@
 #  Parser.
 #
 
+import networkx as nx
+
 # Reads graphs.
 def read_graph (file):
-    graph     = []
-    terminals = []
+    graph     = nx.Graph()
+    terminals = nx.Graph()
     with open(file) as input:
-        for line in input:
-            if line.startswith("E "):
-                graph.append([int(n) for n in line.split()[1:]])
-            elif line.startswith("T "):
-                terminals.append([int(n) for n in line.split()[1:]])
+        graph.add_weighted_edges_from([(int(e.split()[1]),
+                                        int(e.split()[2]),
+                                        int(e.split()[3]))
+                                        for e in input if e.startswith("E ")])
+        terminals.add_nodes_from([int(u.split()[1])
+                                  for u in input if u.startswith("T ")])
     return [graph,terminals]
+
+# Main
+
+g = read_graph("Heuristic/instance001.gr")
 
