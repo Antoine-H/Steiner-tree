ad0296cd0ef3658304b272501cca05de049be8cf
alzatraz <louis.cohen@outlook.fr>
2018-01-24 14:35:32 +0100

    add first_snlution
    add first_solution

--- parser.py
+++ parser.py
@@ -19,6 +19,26 @@ def read_graph (file):
     return [graph,terminals]
 
 
+
+def first_solution(graph,terminals):
+	graph_t = nx.Graph()
+	too_add = []
+	approx_spanning = nx.Graph()
+	for i in range(length(terminal)):
+		for j in range(terminal.size()-i-1):
+			w = nx.shortest_path_length(graph,terminal[i], terminal[j],"weight")
+			too_add.append((terminal[i],terminal[j],w))
+	graph_t.add_weighted_edges_from(too_add)
+	spanning_tree = nx.minimum_spanning_tree(graph_t)
+	for (i,j) in spanning_tree.edges():
+		path = nx.shortest_path(graph,i, j,"weight")
+		for i in range(length(path)-1):
+			approx_spanning.add_edge(path[i],path[i+1])
+	return(approx_spanning)
+
+	
+
+
 if __name__ == '__main__':
     g = read_graph("Heuristic/instance001.gr")
 
