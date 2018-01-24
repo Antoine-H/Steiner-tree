f159f79283ee7b590aacfd3682a77605bf6de9c5
alzatraz <louis.cohen@outlook.fr>
2018-01-24 15:55:39 +0100

    creation local search

--- local_search.py
+++ local_search.py
@@ -25,29 +25,10 @@ def first_solution(graph,terminals):
     return(approx_spanning)
 
 
-def fusion(t0,t1):
-	t_new = nx.compose(t0,t1)
-	edges = shuffle(t_new.edges(data=True))
-	for e in edges:
-		t_new.remove_edge(e)
-		if(not(nx.is_connected(t_new))):
-			t_new.add_adge(e)
-	return(t_new)
+def modification(graph, terminals):
+	
 
-
-def genetic(nb_generation,taille_population):
-	population = random_solution(taille_population)
-	for i in range(nb_generation):
-		population.sort()
-		for j in range(taille_population/2):
-			population.pop()
-		for j in range(taille_population/4):
-			t1 = population[taille_population/4 +j][1]
-			t0 = population[j][0]
-			t_new = fusion(t0,t1)
-			population.append(t_new)
-	population.sort()
-	return(population[0])
+def local_search
 
 if __name__ == '__main__':
     g = read_graph("Heuristic/instance001.gr")
