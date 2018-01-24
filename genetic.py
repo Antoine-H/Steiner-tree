e6d016eea6f1534db26db30edeca8ded314ee9af
alzatraz <louis.cohen@outlook.fr>
2018-01-24 16:55:58 +0100

    first solution deleted
    first solution deleted

--- genetic.py
+++ genetic.py
@@ -5,24 +5,7 @@
 
 import networkx as nx
 import random
-
-
-def first_solution(graph,terminals):
-    graph_t = nx.Graph()
-    too_add = []
-    approx_spanning = nx.Graph()
-    for i in range(len(terminals.nodes())):
-        for j in range(len(terminals.nodes())-i-1):
-            w = nx.shortest_path_length(graph,terminals.nodes()[i], terminals.nodes()[j],"weight")
-            too_add.append((terminals.nodes()[i],terminals.nodes()[j],w))
-    graph_t.add_weighted_edges_from(too_add)
-    spanning_tree = nx.minimum_spanning_tree(graph_t)
-    for (i,j) in spanning_tree.edges():
-        path = nx.shortest_path(graph,i, j,"weight")
-        for i in range(len(path)-1):
-            approx_spanning.add_edge(path[i],path[i+1])
-    return(approx_spanning)
- 
+import local_search
 
 def fusion(t0,t1):
 	t_new = nx.compose(t0,t1)
