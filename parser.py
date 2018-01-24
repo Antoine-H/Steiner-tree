9b9dd218855f4b498aac1145a790f5d238639ce1
antoine <antoine@huchet.me>
2018-01-24 15:03:10 +0100

    First_solution updated

--- parser.py
+++ parser.py
@@ -21,20 +21,20 @@ def read_graph (file):
 
 
 def first_solution(graph,terminals):
-	graph_t = nx.Graph()
-	too_add = []
-	approx_spanning = nx.Graph()
-	for i in range(len(terminals)):
-		for j in range(len(terminals)-i-1):
-			w = nx.shortest_path_length(graph,terminals[i], terminals[j],"weight")
-			too_add.append((terminals[i],terminals[j],w))
-	graph_t.add_weighted_edges_from(too_add)
-	spanning_tree = nx.minimum_spanning_tree(graph_t)
-	for (i,j) in spanning_tree.edges():
-		path = nx.shortest_path(graph,i, j,"weight")
-		for i in range(len(path)-1):
-			approx_spanning.add_edge(path[i],path[i+1])
-	return(approx_spanning)
+    graph_t = nx.Graph()
+    too_add = []
+    approx_spanning = nx.Graph()
+    for i in range(len(terminals.nodes())):
+        for j in range(len(terminals.nodes())-i-1):
+            w = nx.shortest_path_length(graph,terminals.nodes()[i], terminals.nodes()[j],"weight")
+            too_add.append((terminals.nodes()[i],terminals.nodes()[j],w))
+    graph_t.add_weighted_edges_from(too_add)
+    spanning_tree = nx.minimum_spanning_tree(graph_t)
+    for (i,j) in spanning_tree.edges():
+        path = nx.shortest_path(graph,i, j,"weight")
+        for i in range(len(path)-1):
+            approx_spanning.add_edge(path[i],path[i+1])
+    return(approx_spanning)
 
 
 
