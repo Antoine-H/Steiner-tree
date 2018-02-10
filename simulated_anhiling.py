072dc6ac9c4d78c30781566e76707621e02eaf3d
Louis Cohen <louis.cohen@outlook.fr>
2018-02-10 21:38:14 +0100

    ras

--- simulated_anhiling.py
+++ simulated_anhiling.py
@@ -8,6 +8,7 @@ import random
 import local_search as ls 
 import parser
 import matplotlib.pyplot as plt
+import math
 
 def local_search_only_better(nb_step  = 10):
 	cur_sol = (ls.first_solution(graph, terminals))
@@ -42,15 +43,43 @@ def local_search_accept_error(nb_step  = 10,p = .1):
 		l_act.append(act_gain)
 	return(l_act, l_new)
 
+def heat_strategy_linear(nb_step, act_gain, new_gain):
+	r_seuil = math.exp((act_gain-new_gain)/float(10*nb_step))
+	return(r_seuil)
 
-def simulated_anhilling(nb_step = 10, heat_trategy)
+def test_exp():
+	for i in range(10):
+		print(math.exp(-i))
+
+def simulated_anhilling(nb_step = 10, heat_strategy = heat_strategy_linear):
+	cur_sol = (ls.first_solution(graph, terminals))
+	act_gain = ls.gain(cur_sol)
+	l_act = [act_gain]
+	l_new = [act_gain]
+	l_seuils = [0]
+	for nb_step_act in range(nb_step):
+		new_sol = ls.neighbors_of_solution(graph, cur_sol, terminals)
+		new_gain = ls.gain(new_sol)
+		l_new.append(new_gain)
+		r = random.random()
+		r_seuil = heat_strategy(nb_step_act+1, act_gain, new_gain)
+		l_seuils.append(r_seuil)
+		if r<r_seuil:
+			act_gain = new_gain
+			cur_sol = new_sol
+		l_act.append(act_gain)
+	return(l_act, l_new,l_seuils )
+
 
 
 if __name__ == '__main__':
 	g = parser.read_graph("Heuristic/instance039.gr")
 	graph = g[0]
 	terminals = g[1]
-	sol = local_search_accept_error(500,0.01)
+	sol = simulated_anhilling(10,heat_strategy_linear)
 	plt.plot(sol[0],'ro')
 	plt.plot(sol[1],'b^')
 	plt.show()
+	plt.plot(sol[2],'b^')
+	plt.show()
+
