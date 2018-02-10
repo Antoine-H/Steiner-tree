faa1e6cea8575dd27b4bcc28c82f0feb1d360ce8
Louis Cohen <louis.cohen@outlook.fr>
2018-02-10 18:37:40 +0100

    ?

--- simulated_anhiling.py
+++ simulated_anhiling.py
@@ -9,7 +9,7 @@ import local_search as ls
 import parser
 import matplotlib.pyplot as plt
 
-def local_search_dummy(nb_step  = 10):
+def local_search_only_better(nb_step  = 10):
 	cur_sol = (ls.first_solution(graph, terminals))
 	act_gain = ls.gain(cur_sol)
 	l_act = [act_gain]
@@ -26,12 +26,35 @@ def local_search_dummy(nb_step  = 10):
 
 
 
+def local_search_accept_error(nb_step  = 10,p = .2):
+	cur_sol = (ls.first_solution(graph, terminals))
+	act_gain = ls.gain(cur_sol)
+	l_act = [act_gain]
+	l_new = [act_gain]
+	for i in range(nb_step):
+		new_sol = ls.neighbors_of_solution(graph, cur_sol, terminals)
+		new_gain = ls.gain(new_sol)
+		l_new.append(new_gain)
+		if new_gain<act_gain:
+			r = random.random()
+			if r<p:
+				act_gain = new_gain
+				cur_sol = new_sol
+		l_act.append(act_gain)
+	return(l_act, l_new)
+
+
+
+
+
 if __name__ == '__main__':
 	g = parser.read_graph("Heuristic/instance039.gr")
 	graph = g[0]
 	terminals = g[1]
-	sol = local_search_dummy(30)
-	plt.plot(sol[0])
+	sol = local_search_dummy(300)
+	plt.plot(sol[0],'ro')
+	plt.plot(sol[1],'b^')
+	plt.show()
 	print(sol[0])
 	print(sol[1])
 	
