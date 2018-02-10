1d31828a078ada5a7adc26a3b5503b2741071ecf
Louis Cohen <louis.cohen@outlook.fr>
2018-02-10 11:27:29 +0100

    problemes en cours

--- local_search.py
+++ local_search.py
@@ -5,11 +5,22 @@ import parser
 import random
 import matplotlib.pyplot as plt
 
-# Not checked
+def display (graph,name_of_graph):
+    pos=nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph,pos,node_size=30)
+    nx.draw_networkx_edges(graph,pos,width=5,alpha=0.5)
+    plt.axis('off')
+    plt.savefig(name_of_graph)
+    plt.show()
+
+data1 = []
+data2 = []
+
+# First solution : 2-approx
 def first_solution(graph,terminals):
     graph_t = nx.Graph()
     too_add = []
-    approx_spanning = nx.Graph()
+    approx_spanning = nx.Graph() 
     ter = terminals.nodes()
     for n1 in ter:
         for n2 in ter:
@@ -22,18 +33,36 @@ def first_solution(graph,terminals):
         path = nx.shortest_path(graph,i, j,"weight")
         for i in range(len(path)-1):
             data = graph.get_edge_data(path[i],path[i+1])["weight"]
+            data1.append(data)
+            approx_spanning.add_edge(path[i],path[i+1],weight=data)
+    return approx_spanning
+
+edges_tot_2 = []
+
+def better_first_solution(graph,terminals):
+    graph_t = nx.Graph()
+    too_add = []
+    approx_spanning = nx.Graph() 
+    ter = list(terminals.nodes())
+    for i in range(len(ter)-1):
+        n1 = ter[i]
+        n2 = ter[i+1]
+        w = nx.shortest_path_length(graph,n1, n2,"weight")
+        too_add.append((n1,n2,w))
+    graph_t.add_weighted_edges_from(too_add)
+    spanning_tree = nx.minimum_spanning_tree(graph_t)
+    for (i,j) in spanning_tree.edges():
+        path = nx.shortest_path(graph,i, j,"weight")
+        for i in range(len(path)-1):
+            data = graph.get_edge_data(path[i],path[i+1])["weight"]
+            data2.append(data)
+            edges_tot_2.append((path[i],path[i+1]))
             approx_spanning.add_edge(path[i],path[i+1],weight=data)
     return approx_spanning
 
 
-def display (graph,name_of_graph):
-    pos=nx.spring_layout(graph)
-    nx.draw_networkx_nodes(graph,pos,node_size=30)
-    nx.draw_networkx_edges(graph,pos,width=5,alpha=0.5)
-    plt.axis('off')
-    plt.savefig(name_of_graph)
-    plt.show()
 
+#local search algorithm Antoine
 
 def nm_step_dummy_antoine(graph, cur_sol, terminals, n=5, m=5):
     graph_copy = cur_sol.copy()
@@ -57,8 +86,31 @@ def nm_step_dummy_antoine(graph, cur_sol, terminals, n=5, m=5):
 
 
 #Louis version, without indexing problem (edges can't be indexed)
+def is_admissible(subgraph, terminals):
+    n0 = list(terminals.nodes())[0]
+    comp = nx.node_connected_component(subgraph,n0)
+    for n in terminals.nodes():
+        if n not in comp:
+            return(False)
+    return(True)
 
-def edges_adjacent(graph, subgraph):
+#test test 
+########################################
+def test_is_admissible():
+    g = parser.read_graph("Heuristic/instance001.gr")
+    graph = g[0]
+    terminals = g[1]
+    g0 = first_solution(graph, terminals)
+    print(is_admissible(g0,terminals))
+    e = list(g0.edges())
+    random_edge = random.choice(e)
+    g0.remove_edge(*random_edge)
+    print(is_admissible(g0,terminals))
+########################################
+
+
+
+def edges_adjacent(graph, subgraph): #output the list of edges that can be added
     edges_adj = []
     sub_nodes = subgraph.nodes()
     for n1 in sub_nodes:
@@ -67,7 +119,7 @@ def edges_adjacent(graph, subgraph):
                 edges_adj.append((n1,n2))
     return(edges_adj)
 
-def edges_to_delete(graph, terminals):
+def edges_to_delete(graph, terminals): #output the list of edges that can be removed
     edges_to_del  = []
     graph_copy = graph.copy()
     for e in list(graph.edges()):
@@ -77,6 +129,26 @@ def edges_to_delete(graph, terminals):
             edges_to_del.append(e)
         graph_copy.add_edge(*e,weight = data)
     return(edges_to_del)
+
+def add_path(graph, subgraph):
+    list_e = subgraph.edges()
+    n1 = random.choice(list_e)
+    n2 = random.choice(list_e)
+    if (n1!=n2):
+        path  = nx.shortest_path_length(graph,n1, n2,"weight")
+        for i in range(len(path)-1):
+            data = graph.get_edge_data(path[i],path[i+1])["weight"]
+            subgraph.add_edge(path[i],path[i+1],weight=data)
+
+def clean(graph,subgraph,terminals):
+    edges_to_del  = []
+    for e in list(subgraph.edges()):
+        data = graph.get_edge_data(*e)["weight"]
+        subgraph.remove_edge(*e)
+        if(nx.is_connected(graph_copy)):
+            edges_to_del.append(e)
+        graph_copy.add_edge(*e,weight = data)
+    return(edges_to_del)    
 
 def random_add(graph, cur_sol):
     list_e = edges_adjacent(graph ,cur_sol )
@@ -123,13 +195,13 @@ def gain (steiner):
 
 def gain_louis (steiner):
     # Assuming that max eccentricity nodes are terminals
-    w = nx.diameter(steiner)
+    #w = nx.diameter(steiner)
     d = 0
     edges = steiner.edges()
     for e in edges:
         data= steiner.get_edge_data(*e)["weight"]
         d += data
-    return d + w
+    return d 
 
 
 
@@ -152,7 +224,6 @@ def test (heuristic, graph, terminals,nb_test = 5, p=0, new=nx.Graph()):
     #print("le premier gain est : "+ str(gain_louis(new)))
     k = 0
     while k<nb_test:
-        print(nb_test)
         k+=1
         new = local_search (heuristic, graph, new, terminals)
         #print("le gain actuel est : "+ str(gain_louis(new)))
@@ -161,9 +232,13 @@ def test (heuristic, graph, terminals,nb_test = 5, p=0, new=nx.Graph()):
 
 if __name__ == '__main__':
     g = parser.read_graph("Heuristic/instance001.gr")
-    print(len(first_solution(g[0],g[1]).edges()))
-    print("\n\n")
-    print(len(test(nm_step_dummy_louis,g[0],g[1],3, 0.5).edges()))
-    print("\n\n")
-
-    
+    graph = g[0]
+    terminals = g[1]
+    g0 = first_solution(graph, terminals)
+    g1 = better_first_solution(graph, terminals)
+    print(gain_louis(g0))
+    print(data1)
+    print(gain_louis(g1))
+    print(data2)
+    edges_tot_2.sort()
+    print(edges_tot_2)
