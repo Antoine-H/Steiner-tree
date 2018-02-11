32879fab66cb85e838d4cde8109e2db764e4d7dd
Louis Cohen <louis.cohen@outlook.fr>
2018-02-12 00:15:30 +0100

    nouveau truc : submission.py

--- parser.py
+++ parser.py
@@ -4,7 +4,6 @@
 #
 
 import networkx as nx
-import sys
 
 # Reads graphs.
 def read_graph (file):
@@ -22,22 +21,8 @@ def read_graph (file):
     return [graph,terminals]
 
 
-def read_graph_stdin ():
-    graph     = nx.Graph()
-    terminals = nx.Graph()
-    for input in sys.argv[1]:
-        graph.add_weighted_edges_from([(int(e.split()[1]),
-                                        int(e.split()[2]),
-                                        int(e.split()[3]))
-                                        for e in input if e.startswith("E ")])
-    for input in sys.argv[1]:
-        terminals.add_nodes_from([int(u.split()[1])
-                                  for u in input if u.startswith("T ")])
-
-    return [graph,terminals]
-
 
 
 if __name__ == '__main__':
-    g = read_graph_stdin()
+    g = read_graph("Heuristic/instance001.gr")
 
