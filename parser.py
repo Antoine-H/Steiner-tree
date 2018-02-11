c00863ed197ab742787a2c68484986de2100ad4f
antoine <antoine@huchet.me>
2018-02-11 23:58:52 +0100

    Reads from stdin

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@
 #
 
 import networkx as nx
+import sys
 
 # Reads graphs.
 def read_graph (file):
@@ -21,8 +22,22 @@ def read_graph (file):
     return [graph,terminals]
 
 
+def read_graph_stdin ():
+    graph     = nx.Graph()
+    terminals = nx.Graph()
+    for input in sys.argv[1]:
+        graph.add_weighted_edges_from([(int(e.split()[1]),
+                                        int(e.split()[2]),
+                                        int(e.split()[3]))
+                                        for e in input if e.startswith("E ")])
+    for input in sys.argv[1]:
+        terminals.add_nodes_from([int(u.split()[1])
+                                  for u in input if u.startswith("T ")])
+
+    return [graph,terminals]
+
 
 
 if __name__ == '__main__':
-    g = read_graph("Heuristic/instance001.gr")
+    g = read_graph_stdin()
 
